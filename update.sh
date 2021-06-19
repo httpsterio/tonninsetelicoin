944cc4f410944aa3404086f3fc7a4dbd19bdd148
Sami Singh <sami@httpster.io>
2021-06-19 16:09:25 +0300

    the tonnin seteli is now owned by Sami Singh

--- update.sh
+++ update.sh
@@ -3,7 +3,8 @@
 datenow=$(date +%s%N)
 owner=$1
 ledger=ledger.md
-echo -e "$datenow" $1\n >> $ledger 
+echo "$datenow" $1 >> $ledger
+echo >> $ledger
 
 git commit -am "the tonnin seteli is now owned by $1"
 git push origin master
